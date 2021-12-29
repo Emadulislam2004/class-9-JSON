854146c50bc9f6eb8753b6808da0fa5ab20fe164
emadulislam2004 <emadulislam2004@gmail.com>
2021-12-29 22:29:58 +0600

    All file added

---
+++ app.js
@@ -0,0 +1,18 @@
+let studentData   = '[{"ID":1,"name":"Emadul islam","class":11,"gender":"Male","location":"Brahmmon baria","bn":45,"en":60,"math":100,"social":78,"rel":67,"scince":98},{"ID":2,"name":"Akash islam","class":11,"gender":"Male","location":"Dhaka","bn":33,"en":56,"math":78,"social":78,"rel":67,"scince":88},{"ID":3,"name":"Afrin islam","class":11,"gender":"Female","location":"Dhaka","bn":56,"en":65,"math":77,"social":87,"rel":90,"scince":77},{"ID":4,"name":"Tushi islam","class":11,"gender":"Female","location":"Uttora,Dhaka","bn":33,"en":56,"math":88,"social":65,"rel":23,"scince":66},{"ID":5,"name":"Sakib islam","class":11,"gender":"Male","location":"Mirpur,Dhaka","bn":66,"en":43,"math":76,"social":66,"rel":97,"scince":19},{"ID":6,"name":"Mou islam","class":11,"gender":"Female","location":"Sylhet","bn":16,"en":23,"math":36,"social":46,"rel":57,"scince":69},{"ID":7,"name":"Ratul islam","class":11,"gender":"Male","location":"Sylhet","bn":46,"en":53,"math":66,"social":76,"rel":87,"scince":99},{"ID":8,"name":"Tushi islam","class":11,"gender":"Female","location":"Dhaka","bn":96,"en":93,"math":96,"social":96,"rel":97,"scince":99}]'
+let PhoneNumber   = '[{"serialNumber":1,"name":"Father","number":175304703},{"serialNumber":2,"name":"Father","number":175304668},{"serialNumber":3,"name":"Father","number":175304686},{"serialNumber":4,"name":"Father","number":1402138623},{"serialNumber":5,"name":"Father","number":175304640},{"serialNumber":6,"name":"Father","number":175304641},{"serialNumber":7,"name":"mother","number":175304658},{"serialNumber":8,"name":"mother","number":175304685},{"serialNumber":9,"name":"mother","number":175304693},{"serialNumber":10,"name":"mother","number":175304668}]'
+let developerdata = '[{"ID":1,"Name":"Emadul islam","Age":18,"Skill":"Mern stack","Location":"Mirpur, Dhaka","Salary":2000},{"ID":2,"Name":"Akash islam","Age":20,"Skill":"Laravel","Location":"Uttora, Dhaka","Salary":1000},{"ID":3,"Name":"Saif islam","Age":22,"Skill":"Django","Location":"Uttora, Dhaka","Salary":2400},{"ID":4,"Name":"Batas islam","Age":21,"Skill":"Golan","Location":"Demra, Dhaka","Salary":3000},{"ID":5,"Name":"Ariful islam","Age":34,"Skill":"WordPress","Location":"Motijhil, Dhaka","Salary":2200},{"ID":6,"Name":"Emadul islam","Age":18,"Skill":"Java","Location":"Badda, Dhaka","Salary":4000}]'
+
+
+console.log(JSON.parse(studentData));
+console.log(JSON.parse(PhoneNumber));
+console.log(JSON.parse(developerdata));
+
+
+
+
+
+
+
+
+
+
